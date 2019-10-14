35cc6f7ef8f435e78bb1c74773f1ef7e9cfe8c92
davidandraschko <d.andraschko@students.htl-leonding.ac.at>
2019-10-14 18:35:50 +0200

    Method crud() added

--- src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
+++ src/test/java/at/htl/vehicle/rest/VehicleEndpointIT.java
@@ -26,7 +26,7 @@ public class VehicleEndpointIT {
         this.target = client.target("http://localhost:8080/vehicle/api/vehicle");
     }
 
-    @Test
+    /*@Test
     public void fetchVehicle() {
         Response response = this.target.request(MediaType.APPLICATION_JSON).get();
         assertThat(response.getStatus(),is(200));
@@ -36,7 +36,32 @@ public class VehicleEndpointIT {
         JsonObject vehicle = payload.getJsonObject(0);
         assertThat(vehicle.getString("brand"), is ("Opel 42"));
         assertThat(vehicle.getString("type"), is("Commodore"));
+    }*/
+
+    @Test
+    public void crud() {
+        Response response = this.target.request(MediaType.APPLICATION_JSON).get();
+        assertThat(response.getStatus(),is(200));
+        JsonArray payload = response.readEntity(JsonArray.class);
+        System.out.println("payload = " + payload);
+        assertThat(payload,not(empty()));
+
+        JsonObject vehicle = payload.getJsonObject(0);
+        assertThat(vehicle.getString("brand"),equalTo("Opel 42"));
+        assertThat(vehicle.getString("type"), startsWith("Commodore"));
+
+// GET with id
+        JsonObject dedicatedVehicle = this.target
+                .path("43")
+                .request(MediaType.APPLICATION_JSON)
+                .get(JsonObject.class);
+        assertThat(dedicatedVehicle.getString("brand"),containsString("43"));
+        assertThat(dedicatedVehicle.getString("brand"),equalTo("Opel 43"));
+
+        Response deleteResponse = this.target
+                .path("42")
+                .request(MediaType.APPLICATION_JSON)
+                .delete();
+        assertThat(deleteResponse.getStatus(),is(204)); // no content
     }
-
-
 }
